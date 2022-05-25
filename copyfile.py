c9e5bba0c65644ea57e251a9655026f17d98f842
kavya-gm <cdknovartis@gmail.com>
2022-05-25 21:21:11 +0530

    added delete existing object from source bucket

--- copyfile.py
+++ copyfile.py
@@ -1,7 +1,7 @@
 import boto3
-import sys
+
 s3_connect = boto3.resource("s3")
-#print(f'files will be transferred from {sys.argv[1]} to {sys.argv[2]}')
+
 def copy_object(SourceBucketName, FileToCopy, DestinationBucket):
     copy_source = {
     'Bucket': SourceBucketName,
@@ -10,5 +10,5 @@ def copy_object(SourceBucketName, FileToCopy, DestinationBucket):
     bucket = s3_connect.Bucket(DestinationBucket)
     bucket.copy(copy_source, DestinationBucket)
 
-
-    
+def delete_existing_object(SourceBucketName, FileToCopy):
+    s3_connect.Object(SourceBucketName, FileToCopy).delete()
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import boto3
 import sys
 from copyfile import copy_object
+from copyfile import delete_existing_object
 s3_connect = boto3.resource("s3")
-#print(f'files will be transferred from {sys.argv[1]} to {sys.argv[2]}')
+print(f'files will be transferred from {sys.argv[1]} to {sys.argv[3]}')
+SourceBucketName = sys.argv[1]
+FileToCopy = sys.argv[2]
+DestinationBucket = sys.argv[3]
 def main():
-    copy_object("bucket-kavyagm-1","IMG_20210206_180218.jpg", "bucket-kavyagm-2\/test")
+    copy_object(SourceBucketName, FileToCopy, DestinationBucket)
     #copy_object("source bucketname", "object", Destination", "destination bucketname")
+    delete_existing_object(SourceBucketName, FileToCopy)
 
 if __name__ == "__main__":
     main()
